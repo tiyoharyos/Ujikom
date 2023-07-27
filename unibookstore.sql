4815862e7a2213b105b8a46cbac93b61d7095a7e
Tiyo Haryo Subaktiono <87368516+tiyoharyos@users.noreply.github.com>
2023-07-27 09:57:12 +0700

    Update unibookstore.sql

--- unibookstore.sql
+++ unibookstore.sql
@@ -1,24 +1,49 @@
-/*
-SQLyog Ultimate v12.5.1 (64 bit)
-MySQL - 10.4.24-MariaDB : Database - unibookstore
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Jul 2023 pada 04.56
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`unibookstore` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `unibookstore`;
+--
+-- Database: `unibookstore`
+--
 
-/*Table structure for table `buku` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `buku`;
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(20) NOT NULL,
+  `pass` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`username`, `pass`) VALUES
+('Admin', '@Dmin123');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
 
 CREATE TABLE `buku` (
   `ID_Buku` char(20) DEFAULT NULL,
@@ -27,39 +52,47 @@ CREATE TABLE `buku` (
   `Harga` int(20) DEFAULT NULL,
   `Stok` int(15) DEFAULT NULL,
   `Penerbit` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `buku` */
+--
+-- Dumping data untuk tabel `buku`
+--
 
-insert  into `buku`(`ID_Buku`,`Kategori`,`Nama_Buku`,`Harga`,`Stok`,`Penerbit`) values 
-('K1001','NK1','Analisis & Perancangan Sistem Informasi',50000,60,'SP01'),
-('K1002','NK1','Artifical Intelliegence',45000,60,'SP01'),
-('K2003','NK1','Autocad 3 Dimensi',40000,25,'SP01'),
-('B1001','NK2','Bisnis Online',75000,9,'SP01'),
-('K3004','NK1','Cloud Compunting Technology',85000,15,'SP01'),
-('B1002','NK2','Etika Bisnis dan Tanggung Jawab Sosial',67500,20,'SP01'),
-('N1001','NK3','Cahaya Di Penjuru Hati',68000,20,'SP02'),
-('N1002','NK3','Aku Ingin Cerita',48000,12,'SP03');
+INSERT INTO `buku` (`ID_Buku`, `Kategori`, `Nama_Buku`, `Harga`, `Stok`, `Penerbit`) VALUES
+('K1001', 'NK1', 'Analisis & Perancangan Sistem Informasi', 50000, 60, 'SP01'),
+('K1002', 'NK1', 'Artifical Intelliegence', 45000, 60, 'SP01'),
+('K2003', 'NK1', 'Autocad 3 Dimensi', 40000, 25, 'SP01'),
+('B1001', 'NK2', 'Bisnis Online', 75000, 9, 'SP01'),
+('K3004', 'NK1', 'Cloud Compunting Technology', 85000, 15, 'SP01'),
+('B1002', 'NK2', 'Etika Bisnis dan Tanggung Jawab Sosial', 67500, 20, 'SP01'),
+('N1001', 'NK3', 'Cahaya Di Penjuru Hati', 68000, 20, 'SP02'),
+('N1002', 'NK3', 'Aku Ingin Cerita', 48000, 12, 'SP03');
 
-/*Table structure for table `kategori` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `kategori`;
+--
+-- Struktur dari tabel `kategori`
+--
 
 CREATE TABLE `kategori` (
   `ID_Kategori` varchar(20) DEFAULT NULL,
   `Nama_Kategori` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `kategori` */
+--
+-- Dumping data untuk tabel `kategori`
+--
 
-insert  into `kategori`(`ID_Kategori`,`Nama_Kategori`) values 
-('NK1','Keilmuan'),
-('NK2','Bisnis'),
-('NK3','Novel');
+INSERT INTO `kategori` (`ID_Kategori`, `Nama_Kategori`) VALUES
+('NK1', 'Keilmuan'),
+('NK2', 'Bisnis'),
+('NK3', 'Novel');
 
-/*Table structure for table `penerbit` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `penerbit`;
+--
+-- Struktur dari tabel `penerbit`
+--
 
 CREATE TABLE `penerbit` (
   `ID_Penerbit` char(20) DEFAULT NULL,
@@ -67,16 +100,18 @@ CREATE TABLE `penerbit` (
   `Alamat` varchar(50) DEFAULT NULL,
   `Kota` varchar(30) DEFAULT NULL,
   `telepon` char(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `penerbit` */
+--
+-- Dumping data untuk tabel `penerbit`
+--
 
-insert  into `penerbit`(`ID_Penerbit`,`Penerbit`,`Alamat`,`Kota`,`telepon`) values 
-('SP01','Penerbit Informatika','Jl. Buah Batu No.121','Bandung','081322201946'),
-('SP02','Andi Offset','Jl. Suryala IX No. 3','Bandung','087839030688'),
-('SP03','Danendra','Jl Moch. Toha 44','Bandung','02252-1215');
+INSERT INTO `penerbit` (`ID_Penerbit`, `Penerbit`, `Alamat`, `Kota`, `telepon`) VALUES
+('SP01', 'Penerbit Informatika', 'Jl. Buah Batu No.121', 'Bandung', '081322201946'),
+('SP02', 'Andi Offset', 'Jl. Suryala IX No. 3', 'Bandung', '087839030688'),
+('SP03', 'Danendra', 'Jl Moch. Toha 44', 'Bandung', '02252-1215');
+COMMIT;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
